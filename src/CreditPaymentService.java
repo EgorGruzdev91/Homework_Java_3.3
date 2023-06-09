1c90aa60b39e667a1e3ff42b835a908e272de14a
Egor Gruzdev <gruzdev_e.i@mail.ru>
2023-06-09 09:18:03 +0300

    Credit Payment Service Correction 1 09/06/23

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,8 +1,8 @@
 public class CreditPaymentService {
-    public int calculate (int loan_amount, int period_in_year, double procent) {
-        double procent_mounth = procent / 12 / 100;
-        int period_mounth = period_in_year * 12;
-        double payment = loan_amount * procent_mounth * Math.pow(1 + procent_mounth, period_mounth) / ( Math.pow(1 + procent_mounth, period_mounth) - 1);
+    public int calculate (int loanAmount, int periodInYear, double procent) {
+        double procentMounth = procent / 12 / 100;
+        int periodMounth = periodInYear * 12;
+        double payment = loanAmount * procentMounth * Math.pow(1 + procentMounth, periodMounth) / ( Math.pow(1 + procentMounth, periodMounth) - 1);
         return (int) payment;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,34 +4,34 @@ public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
 // Первый расчет
-        int loan_amount = 1_000_000; //изменяется в заявке
-        int period_in_year = 1; //изменяется в заявке
+        int loanAmount = 1_000_000; //изменяется в заявке
+        int periodInYear = 1; //изменяется в заявке
         double procent = 9.99; // для расчета
-        int payment = service.calculate(loan_amount, period_in_year, procent);
-        System.out.println("Сумма кредита: " + loan_amount + " руб.");
-        System.out.println("Срок кредита: " + period_in_year + " год(а)/лет");
+        int payment = service.calculate(loanAmount, periodInYear, procent);
+        System.out.println("Сумма кредита: " + loanAmount + " руб.");
+        System.out.println("Срок кредита: " + periodInYear + " год(а)/лет");
         System.out.println("Ваш ежемесячный платеж составит " + payment + " руб.");
         System.out.println("Процентная ставка: " + procent + "%");
         System.out.println("");
 
 // Второй расчет
-        loan_amount = 1_000_000; //изменяется в заявке
-        period_in_year = 2; //изменяется в заявке
+        loanAmount = 1_000_000; //изменяется в заявке
+        periodInYear = 2; //изменяется в заявке
         procent = 9.99; // для расчета
-        payment = service.calculate(loan_amount, period_in_year, procent);
-        System.out.println("Сумма кредита: " + loan_amount + " руб.");
-        System.out.println("Срок кредита: " + period_in_year + " год(а)/лет");
+        payment = service.calculate(loanAmount, periodInYear, procent);
+        System.out.println("Сумма кредита: " + loanAmount + " руб.");
+        System.out.println("Срок кредита: " + periodInYear + " год(а)/лет");
         System.out.println("Ваш ежемесячный платеж составит " + payment + " руб.");
         System.out.println("Процентная ставка: " + procent + "%");
         System.out.println("");
 
 //Третий расчет
-        loan_amount = 1_000_000; //изменяется в заявке
-        period_in_year = 3; //изменяется в заявке
+        loanAmount = 1_000_000; //изменяется в заявке
+        periodInYear = 3; //изменяется в заявке
         procent = 9.99; // для расчета
-        payment = service.calculate(loan_amount, period_in_year, procent);
-        System.out.println("Сумма кредита: " + loan_amount + " руб.");
-        System.out.println("Срок кредита: " + period_in_year + " год(а)/лет");
+        payment = service.calculate(loanAmount, periodInYear, procent);
+        System.out.println("Сумма кредита: " + loanAmount + " руб.");
+        System.out.println("Срок кредита: " + periodInYear + " год(а)/лет");
         System.out.println("Ваш ежемесячный платеж составит " + payment + " руб.");
         System.out.println("Процентная ставка: " + procent + "%");
     }
